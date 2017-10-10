11495dd547ce184b14bcb507e6cfb6f0b3a16e49
mrgunter51 <mrgunter@crimson.ua.edu>
2017-10-10 05:48:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -27,3 +27,11 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1)
 }
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  return array.pop()
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1)
+}
