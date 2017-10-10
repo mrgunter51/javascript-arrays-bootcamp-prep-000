a216ec0f9ce317e5eac247f923925496bb6949cd
mrgunter51 <mrgunter@crimson.ua.edu>
2017-10-10 05:44:12 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -19,3 +19,7 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[element]
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return array.shift()
+}
